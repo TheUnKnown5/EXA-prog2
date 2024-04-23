7d2a6c82112a4f1a84ef24716bdfd500a1acba01
TheUnKnown5 <sebastiansommer009@gmail.com>
2024-04-23 16:15:13 +0200

    Update 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Kolla i player movement scripted i grupp 6 projektet.
+    // Just change it so it works in a 3D game.
+
+    [Header("Movwment")]
+    [SerializeField] float movementSpeed = 5.0f;
+
+    Vector3 moveInput;
+
+    float forwardMovement;
+    float backwardMovement;
+
+    Rigidbody playerRigidbody;
+    CapsuleCollider playerCollider;
+
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        forwardMovement = moveInput.x;
+        backwardMovement = moveInput.z;
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector3>();
+    }
+
+    void MovePlayer()
+    {
+        playerRigidbody.AddForce(new Vector3(forwardMovement, 0f, backwardMovement));
+
+        if (Mathf.Abs(playerRigidbody.velocity.x) > movementSpeed)
+        {
+            playerRigidbody.velocity = new Vector3(Mathf.Sign(playerRigidbody.velocity.x) * movementSpeed, playerRigidbody.velocity.y,
+                Mathf.Sign(playerRigidbody.velocity.z) * movementSpeed);
+        }
     }
 }
