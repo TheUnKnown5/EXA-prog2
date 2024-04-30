dce88e0e323d46a583aff1a471170885bac8c6be
TheUnKnown5 <sebastiansommer009@gmail.com>
2024-04-30 14:50:27 +0200

    Update 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,14 @@ public class PlayerMovement : MonoBehaviour
     // Kolla i player movement scripted i grupp 6 projektet.
     // Just change it so it works in a 3D game.
 
-    [Header("Movwment")]
+    [Header("Movement")]
     [SerializeField] float movementSpeed = 5.0f;
 
-    Vector3 moveInput;
+    [Header("Jumoing")]
+    [SerializeField] LayerMask JumpableLayers = new LayerMask();
+    [SerializeField] List<GameObject> Jumpables = new List<GameObject>();
+
+    Vector3 moveInput; 
 
     float forwardMovement;
     float backwardMovement;
@@ -49,6 +53,47 @@ public class PlayerMovement : MonoBehaviour
         {
             playerRigidbody.velocity = new Vector3(Mathf.Sign(playerRigidbody.velocity.x) * movementSpeed, playerRigidbody.velocity.y,
                 Mathf.Sign(playerRigidbody.velocity.z) * movementSpeed);
+        }
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            if (CanJump())
+            {
+                PlayerJump();
+            }
+        }
+    }
+
+    void PlayerJump()
+    {
+        
+    }
+    
+    public bool CanJump()
+    {
+        return Jumpables.Count > 0;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (((1 << collision.gameObject.layer) & JumpableLayers) != 0)
+        {
+            Jumpables.Add(collision.gameObject);
+        }
+
+        //if (((1 << collision.gameObject.layer) & JumpableLayers) == 0)
+        //{
+        //    //It wasn't in an ignore layer
+        //}
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (((1 << collision.gameObject.layer) & JumpableLayers) != 0)
+        {
+            Jumpables.Remove(collision.gameObject);
         }
     }
 }
